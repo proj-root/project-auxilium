2e5df43775ab98d266f29c6d8eacad81c7333f45
Wolfyre243 <wolfyreblueflare@gmail.com>
2026-07-23 19:07:07 +0800

    fix port issue again

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -21,6 +21,7 @@ export const auth = betterAuth({
     process.env.CLIENT_URL || '',
     'https://garden.junkaizhang.com',
     'http://localhost:5173',
+    'http://localhost:3000',
     `http://localhost:${port}`,
   ],
 
