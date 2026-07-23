e0a42c5166c50103b931d2cd5a4f4f12933dfa24
Wolfyre243 <wolfyreblueflare@gmail.com>
2026-07-23 16:32:01 +0800

    minor: versioning

--- client/src/components/misc/version.tsx
+++ client/src/components/misc/version.tsx
@@ -6,7 +6,7 @@ export function VersionInfo({ className }: { className?: string; }) {
     <div className={cn('text-muted-foreground text-sm flex flex-row justify-between', className)}>
       <Link to={'/changelog'} target="_blank" title="View Changelog" className="underline">Changelog</Link>
       {/* TODO: Find a more efficient way to handle this */}
-      <h1>v0.3.0-alpha</h1>
+      <h1>v0.4.0-alpha</h1>
     </div>
   )
 }
